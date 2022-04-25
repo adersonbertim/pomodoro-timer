c7b9b51f0ac6a716b850324c8e3503f48e1e9fdf
Aderson Bertim <adersonbertim@gmail.com>
2022-04-25 16:46:44 -0300

    structure

--- pomodoro.sh
+++ pomodoro.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
+#########################################################################
+# Pomotimer = sessionPomo, studyTimer, shortBreak, longBreak, pauseTimer, resetTimer
+# Commands = Sessions, Set timers, Pause, Reset, Help  
+# Display = Numbers, Sessions end, Sessions start, Beeps 
+#########################################################################
+
 
