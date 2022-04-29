dc650924023d7587b28f557e883ba0a4c3c358cb
Aderson Bertim <adersonbertim@gmail.com>
2022-04-28 21:34:15 -0300

    adding colors and first steps

--- pomodoro.sh
+++ pomodoro.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-#########################################################################
-# Pomotimer = sessionPomo, studyTimer, shortBreak, longBreak, pauseTimer, resetTimer
-# Commands = Sessions, Set timers, Pause, Reset, Help  
-# Display = Numbers, Sessions end, Sessions start, Beeps 
-#########################################################################
+function pomotimer () {
+  #adding colors
+  cyan='\e[36m'
+  green='\e[32m'
+  blue='\e[34m'
+  nc='\e[0m'
+  printf "${cyan}Purple and ${green}green and ${blue}blue end${nc}\n"
 
+}
 
+pomotimer
