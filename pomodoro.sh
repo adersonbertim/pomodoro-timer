0a3fdb44640f8b56e6518693d47d1f9a35311560
Aderson Bertim <adersonbertim@gmail.com>
2022-04-29 00:55:20 -0300

    firsts tests

--- pomodoro.sh
+++ pomodoro.sh
@@ -5,9 +5,14 @@ function pomotimer () {
   cyan='\e[36m'
   green='\e[32m'
   blue='\e[34m'
-  nc='\e[0m'
-  printf "${cyan}Cyan and ${green}green and ${blue}blue end${nc}\n"
-
+  nc='\e[0m' # Non Color
+  printf "${cyan}Wellcome to Pomodoro Timer${nc}\n"
+  
+  if [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then 
+    echo "This is a line test"
+    echo -e "line test"
+    echo "test"
+    return
+  fi
 }
 
-pomotimer
