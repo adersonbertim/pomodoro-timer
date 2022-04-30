8c8ad71988c0a90fc18045f8c3e43cadc5d21f9a
Aderson Bertim <adersonbertim@gmail.com>
2022-04-30 00:26:23 -0300

    Pomodoro help

--- pomodoro.sh
+++ pomodoro.sh
@@ -5,14 +5,46 @@ function pomotimer () {
   cyan='\e[36m'
   green='\e[32m'
   blue='\e[34m'
-  nc='\e[0m' # Non Color
-  printf "${cyan}Wellcome to Pomodoro Timer${nc}\n"
-  
+  clear='\e[0m' 
+  printf "${green}Wellcome to Pomodoro Timer${clear}\n"
+
+  # Help command 
   if [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then 
-    echo "This is a line test"
-    echo -e "line test"
-    echo "test"
+    echo "Welcome to helper, see the options below!!"
+    echo "Options:"
+    echo "-v      Version of this Pomodoro."
+    echo "-w      Work timer you want."
+    echo "-b      Break time ajust."
+    echo "-s      Short Break time ajust."
+    echo "-l      Long Break time ajust."
+    echo "-s      Sessions you want to study."
+    echo "Have a nice study!"
     return
   fi
+
+  # Version Command
+  if [[ "$1" == "-v" ]] || [[ "$1" == "--version" ]]; then
+ 
+    return 
+  fi
+
+  # Work command
+  if [[ "$1" == "-w" ]] || [[ "$1" == "--work" ]]; then
+    
+    return
+  fi
+
+  # Short break command
+
+
+
+  # Long break command
+
+
+
+  # Another command
+
+
+
 }
 
